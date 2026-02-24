369221b660bc721bd523b6245ed7bdd9f1c602ba
Yash <yashshukla864@gmail.com>
2026-02-24 12:01:16 +0530

    minor fixes for deployment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config()
+
 import express from 'express';
 import {matchRouter} from "./routes/matches.js";
 import http from "http";
@@ -7,7 +10,9 @@ import {commentaryRouter} from "./routes/commentary.js";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
+// const HOST = process.env.HOST || '0.0.0.0';
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 app.use(express.json());
 const server = http.createServer(app);
@@ -21,8 +26,7 @@ app.get('/', (req, res) => res.send('Hello from Express Server!'));
 app.use('/matches', matchRouter)
 app.use('/matches/:id/commentary', commentaryRouter)
 
-server.listen(PORT, HOST, () => {
-  const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `https://${HOST}:${PORT}`
-  console.log(`Server listening at ${baseUrl}`);
-  console.log(`WebSocket Server listening at ${baseUrl.replace('http','ws')}/ws`)
+server.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}`);
+  console.log(`WebSocket Server listening at /ws`)
 });
